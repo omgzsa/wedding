1b8dc8e60ccaf5ab6e414b27a617c02922c2adfb
omgzsa <zselloandras@gmail.com>
2024-04-07 16:36:02 +0200

    footer component

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>thefooter</div>
+  <div class="relative w-full h-full">
+    <SvgAchilles class="absolute z-30 bg-dark-100" />
+    <Bulbs class="absolute z-40" />
+  </div>
 </template>
 
 <script lang="ts" setup></script>
