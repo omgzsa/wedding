a3604ad58cffa81c4be48755abd22837ac78af0a
omgzsa <zselloandras@gmail.com>
2024-04-08 14:55:51 +0200

    removed extra detail

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="relative">
+  <div class="relative h-0">
     <SvgAchilles class="bg-dark-100" />
     <Bulbs class="absolute top-0 z-40" />
-    <SvgCane class="absolute bottom-0 z-40 left-42 w-44" />
-    <SvgGrass class="absolute bottom-0 z-40 right-5 w-44" />
+    <!-- <SvgCane class="absolute bottom-0 z-40 left-42 w-44" /> -->
+    <!-- <SvgGrass class="absolute bottom-0 z-40 right-5 w-44" /> -->
   </div>
 </template>
 
